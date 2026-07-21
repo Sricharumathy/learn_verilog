04e926ad852d0402718b05273bcda772a3cdc658
SriCharumathy <charushree2122@gmail.com>
2026-07-21 14:09:52 +0000

    Updated

---
+++ grey2bin/grey2bin_tb.v
@@ -0,0 +1,29 @@
+module grey2bin_tb;
+reg [3:0]g;
+wire [3:0]b;
+grey2bin gb(.g(g),.b(b));
+initial begin
+g=4'b0000;#10;
+g=4'b0001;#10;
+g=4'b0011;#10;
+g=4'b0010;#10;
+g=4'b0110;#10;
+g=4'b0111;#10;
+g=4'b0101;#10;
+g=4'b0100;#10;
+g=4'b1100;#10;
+g=4'b1101;#10;
+g=4'b1111;#10;
+g=4'b1110;#10;
+g=4'b1010;#10;
+g=4'b1011;#10;
+g=4'b1001;#10;
+g=4'b1000;#10;
+end
+initial begin
+$monitor("Time=%0t    g=%b b=%b",$time,g,b);
+#200;
+$finish;
+end
+endmodule
+
