b88ae7e9bf8f808b938b54c0852ab5311cd4f4b5
SriCharumathy <charushree2122@gmail.com>
2026-07-29 07:55:41 +0000

    Updated

---
+++ FLIPFLOP_CONVERSIONS/T_2_D/t2d.v
@@ -0,0 +1,12 @@
+module t2d(input clk,rst,d,output reg q);
+wire t;
+assign t=d^q;
+always @(posedge clk) begin
+if(rst)
+q<=1'b0;
+else if(t)
+q<=~q;
+else
+q<=q;
+end
+endmodule
---
+++ FLIPFLOP_CONVERSIONS/T_2_D/t2d_tb.v
@@ -0,0 +1,19 @@
+module t2d_tb;
+reg clk,rst,d;
+wire q;
+t2d dut(clk,rst,d,q);
+initial begin
+clk=0;
+forever #10 clk=~clk;
+end
+initial begin
+$monitor("Time=%0t clk=%b rst=%b d=%b q=%b",$time,clk,rst,d,q);
+rst=1;d=0;
+#20;
+rst=0;d=1;
+#20;
+d=0;
+#20;
+$finish;
+end
+endmodule
